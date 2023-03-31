c42985cb9554c2a1921c7152c591edb5705d4ae6
Juan Carlos Rom (JC) <juancarlosromesanm@gmail.com>
2023-03-30 22:26:13 -0600

    adding funcs

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/juancarlos240/Go_01/varibles"
 )
 
 func main() {
-	varibles.MuestroEnteros()
+	estado, text := varibles.ConviertoaTexto(9889)
+	fmt.Println(estado)
+	fmt.Println(text)
+
 }
